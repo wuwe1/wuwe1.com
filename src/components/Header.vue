d93d93d40c4a2dfc03f250af5d7b1c93bb498b57
wuwe1 <wu_we1@outlook.com>
2022-06-27 00:36:32 -0700

    feat: header & footer

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header flex="" justify-between px-5 py-5>
+    <a href="/">
+      <img
+        w10 h10
+        src="/favicon.jpg"
+      >
+    </a>
+    <nav
+      text-xl
+      flex="~ gap-4" items-center
+    >
+      <router-link to="/posts" title="Blog">
+        <span>Blog</span>
+      </router-link>
+      <router-link to="/projects" title="Projects">
+        <span>Projects</span>
+      </router-link>
+      <a
+        icon-btn
+        rel="noreferrer"
+        href="https://twitter.com/wuwe19"
+        target="_blank" title="Twitter" i-mdi-twitter
+      />
+      <a
+        icon-btn
+        rel="noreferrer"
+        href="https://github.com/wuwe1"
+        target="_blank" title="Github" i-mdi-github
+      />
+      <button icon-btn @click="toggleDark()">
+        <div dark:i-carbon-moon i-carbon-sun />
+      </button>
+    </nav>
+  </header>
+</template>
